12de09939585396661272e59fe3296e8b725e669
Michael Crapse <mikeeybikeey@gmail.com>
2025-01-30 18:45:11 -0800

    Add ApiParameters for MapSolution

--- Tools/ApiParameters.cs
+++ Tools/ApiParameters.cs
@@ -8,3 +8,13 @@ public class Verify {
     /// <summary>The problem instance.</summary>
     public string ProblemInstance { get; set; } = "";
 }
+
+/// <summary>API parameters for the map solution routes.</summary>
+public class MapSolution {
+    /// <summary>The problem instance.</summary>
+    public string ProblemFrom { get; set; } = "";
+    /// <summary>The reduced problem instance.</summary>
+    public string ProblemTo { get; set; } = "";
+    /// <summary>The solution to the problem.</summary>
+    public string ProblemFromSolution { get; set; } = "";
+}
