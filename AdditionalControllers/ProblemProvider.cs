ed9db50b2ca4fbdb488c12ff4d627e603544e888
Michael Crapse <mikeeybikeey@gmail.com>
2025-02-16 11:10:13 -0800

    Add solve route to ProblemProvider

--- AdditionalControllers/ProblemProvider.cs
+++ AdditionalControllers/ProblemProvider.cs
@@ -8,31 +8,42 @@ using API.Interfaces;
 public class ProblemProvider : ControllerBase {
     public static readonly Dictionary<string, Type> Problems = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => typeof(IProblem).IsAssignableFrom(p) && p.IsClass).ToDictionary(x => x.Name, x => x);
     public static readonly Dictionary<string, Type> Verifiers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => typeof(IVerifier).IsAssignableFrom(p) && p.IsClass).ToDictionary(x => x.Name, x => x);
+    public static readonly Dictionary<string, Type> Solvers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => typeof(ISolver).IsAssignableFrom(p) && p.IsClass).ToDictionary(x => x.Name, x => x);
 
+    #pragma warning disable CS8603 // Possible null reference return.
     static IProblem Problem(string name) {
-        #pragma warning disable CS8603 // Possible null reference return.
         return Activator.CreateInstance(Problems[name]) as IProblem; // guaranteed success by `IsAssignableFrom`
-        #pragma warning restore CS8603 // Possible null reference return.
     }
 
     static IVerifier Verifier(string name) {
-        #pragma warning disable CS8603 // Possible null reference return.
         return Activator.CreateInstance(Verifiers[name]) as IVerifier; // guaranteed success by `IsAssignableFrom`
-        #pragma warning restore CS8603 // Possible null reference return.
     }
 
+    static ISolver Solver(string name) {
+        return Activator.CreateInstance(Solvers[name]) as ISolver; // guaranteed success by `IsAssignableFrom`
+    }
+    #pragma warning restore CS8603 // Possible null reference return.
+
     [HttpPost("verify")]
-    public string verify([FromBody]Verify verify) {
+    public string verify(string verifier, [FromBody]Verify verify) {
         // TODO: validate arguments
         return JsonSerializer.Serialize(
-            Verifier(verify.Verifier).verify(verify.ProblemInstance, verify.Certificate).ToString(),
+            Verifier(verifier).verify(verify.ProblemInstance, verify.Certificate).ToString(),
+            new JsonSerializerOptions { WriteIndented = true }
+        );
+    }
+
+    [HttpPost("solve")]
+    public string solve(string solver, [FromBody]string problemInstance) {
+        // TODO: validate arguments
+        return JsonSerializer.Serialize(
+            Solver(solver).solve(problemInstance),
             new JsonSerializerOptions { WriteIndented = true }
         );
     }
 }
 
 public class Verify {
-    public string Verifier { get; set; } = "";
     public string Certificate { get; set; } = "";
     public string ProblemInstance { get; set; } = "";
 }
