ca7f8641779131e3592335024c703828272e7d19
Jetison333 <Jetison333@hotmail.com>
2025-10-29 01:56:20 -0600

    make json serialization work properly

--- AdditionalControllers/ProblemProvider.cs
+++ AdditionalControllers/ProblemProvider.cs
@@ -3,6 +3,8 @@ using System.Text.Json;
 using API.Interfaces;
 using API.Interfaces.JSON_Objects.Graphs;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
+using API.Interfaces.Tools;
+using API.Interfaces.JSON_Objects;
 
 [ApiController]
 [Route("[controller]")]
@@ -78,20 +80,29 @@ public class ProblemProvider : ControllerBase {
     [HttpPost("visualize")]
     public string visualize(string visualizationName, [FromBody] string instance)
     {
-        IVisualization visual = Visualization(visualizationName);
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        options.Converters.Add(new API_JSON_Converter<API_JSON>());
+        API_JSON visual = Visualization(visualizationName).visualize(instance);
         return JsonSerializer.Serialize(
-            visual.visualize(instance),
-            new JsonSerializerOptions { WriteIndented = true }
+            visual,
+            options
         );
     }
 
     [HttpPost("solvedVisualize")]
     public string solvedVisualize(string visualizationName, [FromBody] string instance)
     {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
         IVisualization visual = Visualization(visualizationName);
         return JsonSerializer.Serialize(
             visual.getSolvedVisualization(instance),
-            new JsonSerializerOptions { WriteIndented = true }
+            options
         );
     }
 }
--- Problems/NPComplete/NPC_INDEPENDENTSET/INDEPENDENTSET_Class.cs
+++ Problems/NPComplete/NPC_INDEPENDENTSET/INDEPENDENTSET_Class.cs
@@ -60,6 +60,7 @@ class INDEPENDENTSET : IGraphProblem<IndependentSetBruteForce,IndependentSetVeri
         instance = GInput;
 
         StringParser independentset = new("{((N,E),K) | N is set, E subset N unorderedcross N, K is int}");
+        Console.WriteLine(GInput);
         independentset.parse(GInput);
         nodes = independentset["N"].ToList().Select(node => node.ToString()).ToList();
         edges = independentset["E"].ToList().Select(edge =>
