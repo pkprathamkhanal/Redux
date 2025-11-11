4fe84fdfefecd0163f95f312c04ecc37ccc1efeb
Andrija-Sevaljevic <andrijasevaljevic@isu.edu>
2025-11-11 01:35:10 -0700

    3sat default instance change

--- Problems/NPComplete/NPC_SAT3/SAT3_Class.cs
+++ Problems/NPComplete/NPC_SAT3/SAT3_Class.cs
@@ -14,7 +14,7 @@ class SAT3 : IProblem<Sat3BacktrackingSolver,SAT3Verifier,Sat3DefaultVisualizati
     public string[] contributors {get;} = { "Kaden Marchetti"};
     public string source {get;} = "Karp, Richard M. Reducibility among combinatorial problems. Complexity of computer computations. Springer, Boston, MA, 1972. 85-103.";
     public string sourceLink { get; } = "https://cgi.di.uoa.gr/~sgk/teaching/grad/handouts/karp.pdf";
-    public string defaultInstance {get;} = "(x1 | !x2 | x3) & (!x1 | x3 | x1) & (x2 | !x3 | x1)";
+    public string defaultInstance {get;} = "(x1 | !x2 | x3) & (!x1 | x3 | x1) & (x2 | !x3 | !x1)";
     public Sat3BacktrackingSolver defaultSolver {get;} = new Sat3BacktrackingSolver();
     public SAT3Verifier defaultVerifier {get;} = new SAT3Verifier();
     public Sat3DefaultVisualization defaultVisualization { get; } = new Sat3DefaultVisualization();
