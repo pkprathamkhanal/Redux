8674021e3ad376f9f1b9b37e12c46879d19ec854
Pratham Khanal <pkprathamkhanal@gmail.com>
2025-11-19 09:32:10 -0600

    Update SUBSETSUM_Class.cs
    Just added contributors link to About Us page for now.

--- Problems/NPComplete/NPC_SUBSETSUM/SUBSETSUM_Class.cs
+++ Problems/NPComplete/NPC_SUBSETSUM/SUBSETSUM_Class.cs
@@ -16,6 +16,9 @@ class SUBSETSUM : IProblem<SubsetSumBruteForce,SubsetSumVerifier, DummyVisualiza
     public string source {get;} = "Karp, Richard M. Reducibility among combinatorial problems. Complexity of computer computations. Springer, Boston, MA, 1972. 85-103.";
     public string sourceLink { get; } = "https://cgi.di.uoa.gr/~sgk/teaching/grad/handouts/karp.pdf";
     public string[] contributors {get;} = { "Garret Stouffer, Caleb Eardley"};
+    public string contributorsLink { get; } = "https://redux.portneuf.cose.isu.edu/aboutus";
+
+    
     public static string _defaultInstance { get; } = "{{1,7,12,15} : 28}";
     public string defaultInstance {get;} = _defaultInstance;
     public string instance {get;set;} = string.Empty;
